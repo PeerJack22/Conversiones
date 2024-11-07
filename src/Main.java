e870d7a1b0d0d0a5d4fe89d9ca7a29bb506383de
PeerJack22 <pajocina.11@gmail.com>
2024-11-07 09:42:31 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,74 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // byte a short implicita
+        byte byte1 = 8;
+        short short1 = byte1;
+        System.out.println(short1);
+
+        //byte a char
+        byte byte2 = 45;
+        char char1 = (char) byte2;
+        System.out.println(char1);
+
+        //byte a int
+        byte byte3 = 9;
+        int int1 = byte3;
+        System.out.println(int1);
+
+        //byte a long
+        byte byte4 = 12;
+        long long1 = byte4;
+        System.out.println(long1);
+
+        //byte a float
+        byte byte5 = 24;
+        float float1 = byte5;
+        System.out.println(float1);
+
+        //byte a double
+        byte byte6 = 16;
+        double double1 = byte6;
+        System.out.println(double1);
+
+        //short a byte
+        short short2 = 34;
+        byte byte7 = (byte) short2;
+        System.out.println(byte7);
+
+        //short a char
+        short short3 = 66;
+        char char2 = (char) short3;
+        System.out.println(char2);
+
+        //short a int
+        short short4 = 127;
+        int int2 = short4;
+        System.out.println(int2);
+
+        //short a long
+        short short5 = 23;
+        long long2 = short5;
+        System.out.println(long2);
+
+        //short a float
+        short short6 = 16;
+        float float2 = short6;
+        System.out.println(float2);
+
+        //short a double
+        short short7 = 45;
+        double double2 = short7;
+        System.out.println(double2);
+
+        //char a byte
+        char char3 = '@';
+        byte byte8 = (byte) char3;
+        System.out.println(byte8);
+
+        //char a short
+        
+
+
+    }
+}
